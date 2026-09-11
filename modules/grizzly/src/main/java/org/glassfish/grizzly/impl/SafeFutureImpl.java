f29e81eeb1d136bfed6c56fbc9cf5c846b420db8
Bongjae Chang <bongjae.chang@gmail.com>
2026-09-11 11:01:57 +0900

    Merge pull request #2307 from jcalcote/fix-2306-future-completion
    Fix premature SafeFutureImpl completion detection

--- modules/grizzly/src/main/java/org/glassfish/grizzly/impl/SafeFutureImpl.java
+++ modules/grizzly/src/main/java/org/glassfish/grizzly/impl/SafeFutureImpl.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation.
  * Copyright (c) 2010, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -285,7 +286,8 @@ public class SafeFutureImpl<R> implements FutureImpl<R> {
         private Throwable exception;
 
         private boolean ranOrCancelled() {
-            return (getState() & (RAN | CANCELLED)) != 0;
+            final int state = getState();
+            return state == RAN || state == CANCELLED;
         }
 
         /**
